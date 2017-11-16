39623639d4c59d7acc35392c757cc64dd30f908e
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-11-16 17:32:53 +0530

    Create LinkedLists_DetectACycle.cpp

---
+++ Data Structures/LinkedLists_DetectACycle.cpp
@@ -0,0 +1,34 @@
+
+
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    
+    if(!head)
+        return 0;
+    
+    Node *slwptr = head, *fstptr = head;
+    
+    while(slwptr && fstptr && fstptr->next)
+    {
+        slwptr = slwptr->next;
+        fstptr = fstptr->next->next;
+        
+        if(slwptr == fstptr)
+            return 1;
+    }
+    return 0;
+}
+
+
+
